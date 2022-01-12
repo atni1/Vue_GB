6457d1d9df2b5ec838f9a0e83cc62eb5539f1237
atni1 <babenkoanton25@gmail.com>
2022-01-12 18:10:53 +0600

    Lesson-3

---
+++ l3/src/components/AddNewCost.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="showForm" :class="$style.settings__btn">Add new cost</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AddNewCost',
+  data: () => ({
+    showComp: false,
+  }),
+  methods: {
+    showForm() {
+      this.$emit('show-form');
+    },
+  },
+};
+</script>
+
+<style module lang="scss">
+.settings__btn {
+  padding: 12px 30px;
+  background-color: rgba(0, 199, 60, 0.671);
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  margin: 15px 0;
+}
+</style>
---
+++ l3/src/components/AddPaymentForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <input type="text" v-model="amount" placeholder="Amount">
+        <input type="text" v-model="type" placeholder="Type of cost">
+        <input type="datetime" v-model="date" placeholder="Date costs">
+        <button @click="addCosts">ADD</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AddPaymentForm',
+  data: () => ({
+    amount: '',
+    type: '',
+    date: '',
+  }),
+  computed: {
+    getCurrentDate() {
+      const today = new Date();
+      const day = today.getDate();
+      const month = today.getMonth() + 1;
+      const year = today.getFullYear();
+      return `${day}.${month}.${year}`;
+    },
+  },
+  methods: {
+    addCosts() {
+      const data = {
+        amount: +this.amount,
+        type: this.type,
+        date: this.date || this.getCurrentDate,
+      };
+      this.$emit('add-costs', data);
+    },
+  },
+};
+</script>
+
+<style module lang="scss">
+
+</style>
